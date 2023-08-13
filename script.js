00539096392a7cf710b9c3d20755cfd24b69827e
EMMADEKO10 <emmanueldeko64@gmail.com>
2023-08-13 15:04:54 +0100

    Ajouter le chiffre au champ de saisie

--- script.js
+++ script.js
@@ -15,7 +15,15 @@ inputcontenairs.disabled = true;
 
 // Ajouter un gestionnaire d'événement pour chaque bouton
 buttons.forEach(button => {
-    
+    button.addEventListener('click', event => {
+        const buttonValue = button.textContent;
+
+        // Vérifier si le bouton est un chiffre ou un opérateur
+        if (buttonValue >= '0' && buttonValue <= '9') {
+            // Ajouter le chiffre au champ de saisie
+            inputcontenairs.value += buttonValue;
+        } 
+    });
 });
 
 
