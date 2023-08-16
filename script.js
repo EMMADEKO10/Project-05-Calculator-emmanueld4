e279db44197c6d9f7adf7bdf0f44b55dc24ba289
EMMADEKO10 <emmanueldeko64@gmail.com>
2023-08-16 19:45:23 +0100

    Retour à l'ancienne version

--- script.js
+++ script.js
@@ -2,223 +2,162 @@ import { calculate } from './calculator.js';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
+
 // Récupérer les éléments de la calculatrice
 const inputcontenairs = document.getElementById('input');
 const buttons = document.querySelectorAll('button');
 const historique = document.querySelector('p');
 let expression = '';
-let affichageHist = "";
-let operator = "";
-var verifSigne = false;
-var expreChar = "";
-var value = "";
 
 //Bloquer le display pour n'entrer que les valeurs via les bouttons.
 inputcontenairs.disabled = true;
 // historique.style.display = none;
 
+
 // Ajouter un gestionnaire d'événement pour chaque bouton
 buttons.forEach(button => {
-    button.addEventListener('click', event => {
-        
-        const buttonValue = button.textContent;
-    
-        // Vérifier si le bouton est un chiffre ou un opérateur
+    button.addEventListener('click', event => {
+        const buttonValue = button.textContent;
 
-        if (buttonValue >= '0' && buttonValue <= '9') {
-            // Ajouter le chiffre au champ de saisie
-            if(historique.textContent.includes("+")||historique.textContent.includes("-")||historique.textContent.includes("÷") ||historique.textContent.includes("")){
-                value += buttonValue;
-            }
-            
-            if (inputcontenairs.value === '0' && buttonValue === '0') {
-                inputcontenairs.value = inputcontenairs.value;
-                affichageHist = buttonValue;
-                verifSigne = false;
-                value = buttonValue;
-            } else if(inputcontenairs.value === '0'){
-                inputcontenairs.value = buttonValue;
-                // historique.textContent = buttonValue;
-                affichageHist = buttonValue;
-                value = buttonValue;
-            }
-            else{
-                // Gérer lorsque l'utilisateur veut entre un autre exercice.
+        // Vérifier si le bouton est un chiffre ou un opérateur
 
-                if (!historique.textContent.includes('=')) {
-                    expression += buttonValue;
-                    inputcontenairs.value += buttonValue;
-                    // affichageHist += buttonValue;
-                    // historique.textContent += buttonValue;
-                    affichageHist += buttonValue;
-                     operator += buttonValue;
-                     
-                } else {
-                    expreChar +=buttonValue; 
-                    inputcontenairs.value = '';
-                    historique.textContent = '';
-                    affichageHist = ""
-                    expression = '';
-                    inputcontenairs.value += buttonValue;
-                    // historique.textContent += buttonValue;
-                    affichageHist += buttonValue;
-                    expression += buttonValue;
-                     operator += buttonValue;
-                }
-                // affichageHist += buttonValue;
-            }
+        if (buttonValue >= '0' && buttonValue <= '9') {
+            // Ajouter le chiffre au champ de saisie
+            if(inputcontenairs.value === '0' && buttonValue ==='0'){
+                inputcontenairs.value = inputcontenairs.value;
+            }else{
+                if (!historique.textContent.includes('=')) {
+                    expression += buttonValue;
+                    inputcontenairs.value += buttonValue;
+                    historique.textContent += buttonValue;
+                } else {
+                    inputcontenairs.value = '';
+                    historique.textContent = '';
+                    expression = '';
+                    inputcontenairs.value += buttonValue;
+                    historique.textContent += buttonValue;
+                    expression += buttonValue;
+                }
+    
+            }
 
-        } else {
-            // Vérifier si le champ de saisie n'est pas vide
-            
-            operator += buttonValue;
-            if (inputcontenairs.value !== '') {
-                //  Vérifier l'opérateur et effectuer le calcul correspondant
-                switch (buttonValue) {
-                    case '+':
-                        expreChar +=buttonValue;  
-                        inputcontenairs.value = '';
-                        verifSigne = true;
-                        if (!historique.textContent.includes('=')) {
-                            historique.textContent = affichageHist + " + ";
-                        } else {
-                            historique.textContent = expression + ' + ';
-                        }
-                        affichageHist = "";
-                        expression += '+';
-                        event.preventDefault();
-                        break;
-                    case '-':
-                        expreChar +=buttonValue; 
-                        verifSigne = true;
-                        inputcontenairs.value = '';
-                        if (!historique.textContent.includes('=')) {
-                            historique.textContent = affichageHist +  " - "
-                        } else {
-                            historique.textContent = expression + ' - '
-                        }
-                        affichageHist = "";
-                        expression += '-';
-                        event.preventDefault();
-                        break;
-                    case '×':
-                        expreChar +="*"; 
-                        verifSigne = true;
-                        inputcontenairs.value = '';
-                        if (!historique.textContent.includes('=')) {
-                            historique.textContent = affichageHist + " * "
-                        } else {
-                            historique.textContent = expression + ' * '
-                        }
-                        affichageHist = "";
-                        expression += '*';
-                        event.preventDefault();
-                        break;
-                    case '÷':
-                        expreChar +="/"; 
-                        verifSigne = true;
-                        inputcontenairs.value = '';
-                        if (!historique.textContent.includes('=')) {
-                            historique.textContent = affichageHist + " ÷ "
-                        } else {
-                            historique.textContent = expression + ' ÷ '
-                        }
-                        affichageHist = "";
-                        expression += '/';
-                        event.preventDefault();
-                        break;
-                    case '%':
-                        inputcontenairs.value = eval(expression) / 100;
-                        historique.textContent = inputcontenairs.value + " % "
-                        event.preventDefault();
-                        break;
+           
+        } else {
+            // Vérifier si le champ de saisie n'est pas vide
 
-                    case '=':
-                        if(historique.textContent != ''){
-                            if (!historique.textContent.includes('=')) {
-                                //// Évaluer l'expression mathématique et afficher le résultat
-                                if(!verifSigne) {
-                                    historique.textContent = "";
-                                }else{
-                                    historique.textContent += affichageHist + " = "
-                                    // historique.insertAdjacentHTML('beforeend', '<br>');
-                                    inputcontenairs.value = eval(expression);
-                                    // const valeurAdd = eval(expression);
-                                    expression = eval(expression);
-                                    
-                                }
-                               
-                                event.preventDefault();
-                                break;
-                            } else {
-                                // expression = eval(valeurAdd + expression);
-                                historique.textContent =expression + expreChar + value + " = "
-                                expression = eval(expression + expreChar + value);
-                                inputcontenairs.value = eval(expression); 
-                                event.preventDefault();
-                                break;
-                            }
-                        }else{
-                            inputcontenairs.value = buttonValue;
-                            // historique.textContent = "";
-                            // inputcontenairs.value = "";
-                        }
-                        
+            if (inputcontenairs.value !== '') {
+                //  Vérifier l'opérateur et effectuer le calcul correspondant
+                switch (buttonValue) {
+                    case '+':
+                        // if(inputcontenairs.value.includes(':','*', '+', ))
+                        inputcontenairs.value = '';
+                        if (!historique.textContent.includes('=')){
+                            historique.textContent += " + "
+                        }else{
+                            historique.textContent = expression + '+'
+                        }
+                        
+                        expression += '+';
+                        event.preventDefault();
+                        break;
+                    case '-':
+                        inputcontenairs.value = '';
+                        if (!historique.textContent.includes('=')){
+                            historique.textContent += " - "
+                        }else{
+                            historique.textContent = expression + '-'
+                        }
+                        
+                        expression += '-';
+                        event.preventDefault();
+                        break;
+                    case '×':
+                        inputcontenairs.value = '';
+                        if (!historique.textContent.includes('=')){
+                            historique.textContent += " * "
+                        }else{
+                            historique.textContent = expression + '*'
+                        }
+                        
+                        expression += '*';
+                        event.preventDefault();
+                        break;
+                    case '÷':
+                        inputcontenairs.value = '';
+                        if (!historique.textContent.includes('=')){
+                            historique.textContent += " ÷ "
+                        }else{
+                            historique.textContent = expression + '/'
+                        }
+                        
+                        expression += '/';
+                        event.preventDefault();
+                        break;
+                    case '%':
+                        inputcontenairs.value = eval(expression) / 100;
+                        historique.textContent += "% "
+                        event.preventDefault();
+                        break;
 
-                    case 'AC':
-                        //Effacer le champ de saisie
-                        value = "";
-                        expreChar = "";
-                        inputcontenairs.value = '';
-                        historique.textContent = " ";
-                        expression = '';
-                        affichageHist = "";
-                        event.preventDefault();
-                        break;
-                    case 'C':
-                        // Supprimer le dernier caractère du champ de saisie
-                        affichageHist = affichageHist.slice(0, -1);
-                        inputcontenairs.value = inputcontenairs.value.slice(0, -1);
-                        expression = expression.slice(0, -1);
-                        
-                        event.preventDefault();
-                        break;
-                    case '+/-':
-                        // Inverser le signe du nombre dans le champ de saisie
-                        inputcontenairs.value = -inputcontenairs.value;
-                        expression = -expression;
-                        affichageHist = -affichageHist;
-                        event.preventDefault();
-                        break;
+                    case '=':
+                        if(!historique.textContent.includes('=')){
+                             //// Évaluer l'expression mathématique et afficher le résultat
+                        historique.textContent += " = " 
+                        // historique.insertAdjacentHTML('beforeend', '<br>');
+                        inputcontenairs.value = eval(expression);
+                        const valeurAdd = eval(expression);
+                        expression = eval(expression);
+                        event.preventDefault();
+                        break;
+                        }else {
+                            expression = eval(valeurAdd + expression);
+                            break;
+                        }
+                       
+                       
+                    case 'AC':
+                        //Effacer le champ de saisie
+                        inputcontenairs.value = '';
+                        historique.textContent = " ";
+                        expression = '';
+                        event.preventDefault();
+                        break;
+                    case 'C':
+                        // Supprimer le dernier caractère du champ de saisie
 
-                    case '.':
+                        inputcontenairs.value = inputcontenairs.value.slice(0, -1);
+                        expression = inputcontenairs.value;
+                        event.preventDefault();
+                        break;
+                    case '+/-':
+                        // Inverser le signe du nombre dans le champ de saisie
+                        inputcontenairs.value = -inputcontenairs.value;
+                        expression = -expression;
+                        event.preventDefault();
+                        break;
 
-                        if (!inputcontenairs.value.includes('.')) {
-                            inputcontenairs.value += '.';
-                           affichageHist += ".";
-                            expression += '.';
-                        }
-                }
-            } else if (buttonValue === 'AC') {
-                inputcontenairs.value = '';
-                historique.textContent = " ";
-                expression = '';
-                affichageHist = "";
-            } else if (buttonValue >= '0' && buttonValue <= '9' && historique.textContent.includes('=')) {
-                affichageHist = "";
-                expression = calculate(expression);
-                historique.textContent = expression;
-                expression += buttonValue;
+                    case '.':
 
-            }
-            // else if (inputcontenairs.textContent === "" && isNaN(historique.textContent.slice(-1))){
-            //         historique.textContent = historique.textContent.slice(0, -1) + "+";
-            //         operator = operator.slice(0, -1) + "+";
-            //         affichageHist = "";
-            //         // event.preventDefault();
-            //         }
-        }
-        console.log(expression);
-    });
+                        if (!inputcontenairs.value.includes('.')) {
+                            inputcontenairs.value += '.';
+                            historique.textContent += ".";
+                            expression += '.';
+
+                        }
+                }
+            } else if (buttonValue === 'AC') {
+                inputcontenairs.value = '';
+                historique.textContent = " ";
+                expression = '';
+            } else if (buttonValue >= '0' && buttonValue <= '9' && historique.textContent.includes('=')) {
+
+                expression = calculate(expression);
+                historique.textContent = expression;
+
+            }
+        }
+        console.log(expression);
+    });
 });
+
 
